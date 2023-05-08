f5a37b45732c52851fda3bc21f4972a3d81c3e5a
unknown <iamaidamina@gmail.com>
2023-05-08 00:13:27 -0500

    adding changes to GUI

--- app/src/main/java/frontend/MainFrame.java
+++ app/src/main/java/frontend/MainFrame.java
@@ -1,5 +1,7 @@
 package frontend;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.*;
 
@@ -8,15 +10,24 @@ public class MainFrame extends JFrame{
 
     public MainFrame()
     {
+    	
+    	Toolkit screen = Toolkit.getDefaultToolkit();
+    	Dimension screenSize = screen.getScreenSize();
+    	int screenHeight = screenSize.height;
+    	int screenWidth = screenSize.width;
         //Set dimension of the frame
-        setSize(new Dimension(500, 300));
+        setSize(screenWidth/2, screenHeight/2);
+        setLocation(screenWidth/4, screenHeight/4	);
+        setTitle("Maze");
+        String IconImagePath = "C:\\Users\\crist\\Downloads\\Goku-Smart\\app\\src\\main\\resources\\images\\maze.png";
+        Image mazeIcon = screen.getImage(IconImagePath);
+        setIconImage(mazeIcon);
         //Set close when click X
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setTitle("Goku Smart");
+  
         setVisible(true);
-        //Maximizado hasta cubrir toda la pantalla
-        //setExtendedState(Frame.MAXIMIZED_BOTH);
+
     }
     
 }
